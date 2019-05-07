b6d040391aec4262eba19fac4cb49e09e47804a6
Z-S-Burke <zach@zsburke.com>
2019-05-07 10:04:27 +0200

    backend displaying on local and frontend included

---
+++ src/main/java/com/codeoftheweb/salvo/PlayerRepository.java
@@ -0,0 +1,9 @@
+package com.codeoftheweb.salvo;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+//    List<Player> findByUsername(String username);
+}
+
